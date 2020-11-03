610933105a0370a73e693e1dd582395512d6bfcb
Ron <ronniedelarosa@hotmail.com>
2020-11-03 15:26:27 -0500

    server side

---
+++ express/readme.md
@@ -0,0 +1,20 @@
+express
+
+sails
+    - offers an ORM sub framework
+
+koa
+
+
+Why to use socket.io?
+    - to get data in a specified time like in a second which called term pulling
+    - socket.io is websocket library which we can notify client
+    - push notification to client or multiple clients and this works the same way
+    - default to pulling for older browser not supporting websocket
+
+Which code should come instead of the ??? placeholder to start the server-side Socket.io functionality? http.listen
+
+
+socket.io
+    - bidirectional communication
+    - has two points: client side library runs on browser and server side library runs on nodejs
---
+++ express/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http)
+
+app.use(express.static(__dirname));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+const messages = [
+    { name: 'Tim', message: 'Hi' },
+    { name: 'Jane', message: 'Hello' }
+];
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+    messages.push(req.body);
+    // inform all clients
+    io.emit('message', req.body);
+    res.sendStatus(200);
+});
+
+io.on('connection', (socket) => {
+    console.log('Connected', { socket });
+});
+// instead of just app.listen we can no longer serve express by the backend any longer
+// to run socket.io and express change to http.listen
+const server = http.listen(3000, () => {
+    console.log('server is listening on port', server.address().port);
+});
